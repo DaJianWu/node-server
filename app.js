f4cbd8cf05b29a46eebdf4d0a530d53488579250
wudajian <wudajian@mistong.com>
2022-07-07 18:01:09 +0800

    chore: 🐳 cloud server

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 const http = require('http');
+const fs = require("fs");
 
 const hostname = '127.0.0.1';
+// const hostname = '0.0.0.0';
 const port = 3000;
 
+function readFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "binary", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(data);
+    })
+  })
+};
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
+  // res.setHeader("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+
+  if (req.url.indexOf('/dist/') !== - 1) {
+    console.log('读取静态文件中...');
+    readFile('.' + req.url).then((data) => {
+      res.write(data, "binary");
+      res.end();
+    })
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  }
 });
 
 server.listen(port, hostname, () => {
